a39799f66cfd222cf3037aaef6eb0a01a1a4b538
yury-1980 <yuryman2019@mail.ru>
2025-02-14 12:01:31 +0300

    Примеры задач с решениями

--- src/stream/f.java
+++ src/stream/f.java
@@ -9,7 +9,7 @@ public class f {
 }
 
 
-public class FunctionalInterfacesExample {
+class FunctionalInterfacesExample {
     public static void main(String[] args) {
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
         // Predicate: фильтруем чётные числа
